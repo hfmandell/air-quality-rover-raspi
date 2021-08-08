c23bce11263812e3ec1a5a770d204318e24d9c6d
hfmandell <hfmandell@gmail.com>
2021-08-08 09:36:25 -0700

    change position.txt to position.csv

--- gps.py
+++ gps.py
@@ -1,7 +1,7 @@
 import serial
 import datetime
 
-port="/dev/cu.usbmodem14201"
+port="/dev/cu.usbmodem14201" #or whatever port works on the device
 gps = serial.Serial(port, baudrate=9600)
 
 print(gps)
@@ -12,7 +12,7 @@ while True:
     if data[0] == "b'$GPRMC":
         # only accept valid data
         if data[2] == "A":
-            with open("position.txt", "a") as f_pos:
+            with open("data/raw/position.csv", "a") as f_pos:
                 # only print lat & long
                 now = datetime.datetime.now()
                 f_pos.write(str(now) + ", " + data[3] + ", " + data[5] + "\n")
