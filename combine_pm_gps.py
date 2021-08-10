023d02c1440ee15be9007ff07f7defff866f1e6b
hfmandell <hfmandell@gmail.com>
2021-08-10 00:33:51 -0700

    remove excess columns and cleaned column names

--- combine_pm_gps.py
+++ combine_pm_gps.py
@@ -14,7 +14,23 @@ def combine_pm_gps_csvs():
                             on='timestamp', how='right',
                             lsuffix='_gps', rsuffix='_pm').dropna()
 
+    gps_and_pm_df = gps_and_pm_df.reset_index()
+    gps_and_pm_df = gps_and_pm_df.drop(['index'], axis = 1)
+
+    gps_and_pm_df['lat'] = gps_and_pm_df[' lat']
+    gps_and_pm_df = gps_and_pm_df.drop([' lat'], axis = 1)
+
+    gps_and_pm_df['long'] = gps_and_pm_df[' long']
+    gps_and_pm_df = gps_and_pm_df.drop([' long'], axis = 1)
+
+    gps_and_pm_df = gps_and_pm_df.drop(['pm1_cf'], axis = 1)
+    gps_and_pm_df = gps_and_pm_df.drop(['pm25_cf'], axis = 1)
+    gps_and_pm_df = gps_and_pm_df.drop(['pm10_cf'], axis = 1)
+
     print(gps_and_pm_df)
+    print(gps_and_pm_df['timestamp'].values[0])
+    print(gps_and_pm_df['timestamp'].values[-1])
+    print(gps_and_pm_df['pm1'].values[-1])
 
     gps_and_pm_df.to_csv('data/processed/pm_with_gps.csv')
 
